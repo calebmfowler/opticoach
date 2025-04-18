b9f3fd55666969c4e3b9888129040e04cede2750
Caleb Fowler <caleb.fowler.atx@gmail.com>
2025-04-18 00:43:33 -0500

    Records

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,6 @@
 from aggregate import Aggregator
 from copy import deepcopy
+from numpy import array as nparr
 from pandas import Series, to_numeric, DataFrame, json_normalize, read_json
 from utilities import bound, load_json, serialize_dictionary, tabulate_dictionary
 
@@ -39,28 +40,61 @@ class Preprocessor:
         coachHistoryJSON = load_json('files/coach_history.json')
         
         school_coach_year = bound(tabulate_dictionary(coachHistoryJSON, columnDepth=3, indexDepth=1, valueDepth=0), startYear, endYear)
-        print(school_coach_year)
+        # print('school_coach_year\n', school_coach_year)
         # Need to remap to schools to consistent names
         role_coach_year = bound(tabulate_dictionary(coachHistoryJSON, columnDepth=3, indexDepth=1, valueDepth=2), startYear, endYear)
-        print(role_coach_year)
+        # print('role_coach_year\n', role_coach_year)
         # Need to remap to roles to consistent names
 
         heismanJSON = load_json('files/heismans.json')
         heismanSchool_year = bound(serialize_dictionary(heismanJSON, indexDepth=0, valueDepth=2), startYear, endYear)
         # Need to remap to schools to consistent names
         heismanSchool_year = heismanSchool_year.reindex(school_coach_year.index)
-        heisman_coach_year = bound(school_coach_year.eq(heismanSchool_year, axis=0), startYear, endYear).astype(int)
-        print(heisman_coach_year)
+        heismanCoach_year = bound(school_coach_year.eq(heismanSchool_year, axis=0), startYear, endYear).astype(int)
+        # print('heismanCoach_year\n', heismanCoach_year)
         
         finalPollsJSON = load_json('files/final_polls.json')
         rank_school_year = bound(tabulate_dictionary(finalPollsJSON, columnDepth=(2, None), indexDepth=0, valueDepth=1), startYear, endYear)
         rank_school_year = rank_school_year.apply(to_numeric, errors='coerce').astype('Int64')
-        print(rank_school_year)
+        # print('rank_school_year\n', rank_school_year)
 
         recordsJSON = load_json('files/records.json')
-        record = bound(tabulate_dictionary(recordsJSON, columnDepth=0, indexDepth=1, valueDepth=(2, None)), startYear, endYear)
-
-        # convert all tables to numerical datatypes
+        recordsDF = bound(tabulate_dictionary(recordsJSON, columnDepth=0, indexDepth=1, valueDepth=(2, None)), startYear, endYear)
+        offense = lambda record : [int(game[1].split('-')[0]) for game in record]
+        defense = lambda record : [int(game[1].split('-')[1]) for game in record]
+        def offensiveScorer(record):
+            try:
+                return sum(nparr(offense(record), dtype=int))
+            except:
+                return 0
+        def defensiveScorer(record):
+            try:
+                return sum(nparr(defense(record), dtype=int))
+            except:
+                return 0
+        def winCounter(record):
+            try:
+                return sum(nparr(
+                    [offense > defense for offense, defense in zip(offense(record), defense(record))]
+                , dtype=int))
+            except:
+                return 0
+        def lossCounter(record):
+            try:
+                return sum(nparr(
+                    [offense < defense for offense, defense in zip(offense(record), defense(record))]
+                , dtype=int))
+            except:
+                return 0
+        
+        offensiveScore_school_year = recordsDF.applymap(offensiveScorer)
+        # print('offensiveScore_school_year\n', offensiveScore_school_year)
+        defensiveScore_school_year = recordsDF.applymap(defensiveScorer)
+        # print('defensiveScore_school_year\n', defensiveScore_school_year)
+        wins_school_year = recordsDF.applymap(winCounter)
+        # print('wins_school_year\n', wins_school_year)
+        losses_school_year = recordsDF.applymap(lossCounter)
+        # print('losses_school_year\n', losses_school_year)
 
         self.preprocessedFiles = {
             "trainX": "files/trainX.pkl",
